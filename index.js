3d402484e6ba8febc4694f3622310a395527f7f9
Manuel Rössler <fxmanu@users.noreply.github.com>
2017-06-10 12:48:26 +0200

    Improve code style and error handling in 'getEvents' method

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-var https = require("https");
-var parseString = require('xml2js').parseString;
-var moment = require('moment');
-var ical = require('ical.js');
+const https = require('https');
+const parseString = require('xml2js').parseString;
+const moment = require('moment');
+const ical = require('ical.js');
 
 
   /**
@@ -16,13 +16,12 @@ var ical = require('ical.js');
    * @param  {function} cb
 
    */
-var updateEvent = function (event, url, user, pass, method, cb) {
-
-    var urlparts = /(https?)\:\/\/(.*?):?(\d*)?(\/.*\/?)/gi.exec(url);
-    var protocol = urlparts[1];
-    var host = urlparts[2];
-    var port = urlparts[3] || (protocol === "https" ? 443 : 80);
-    var path = urlparts[4] + event.key;
+const updateEvent = function (event, url, user, pass, method, cb) {
+    const urlparts = /(https?)\:\/\/(.*?):?(\d*)?(\/.*\/?)/gi.exec(url);
+    const protocol = urlparts[1];
+    const host = urlparts[2];
+    const port = urlparts[3] || (protocol === 'https' ? 443 : 80);
+    const path = urlparts[4] + event.key;
 
     /*
 
@@ -46,52 +45,53 @@ var updateEvent = function (event, url, user, pass, method, cb) {
 
     */
 
-    var body = 'BEGIN:VCALENDAR\n' +
+    let body = `${'BEGIN:VCALENDAR\n' +
                'BEGIN:VEVENT\n' +
-               'UID:' + event.key + '\n' +
-               'SUMMARY:' + event.summary + '\n';
+               'UID:'}${event.key}\n` +
+               `SUMMARY:${event.summary}\n`;
 
-    var _startDateBody, _endDateBody;
+    let _startDateBody;
+    let _endDateBody;
 
-    var format_allDay = "YYYYMMDDTHHmms";
-    var format_singleEvent = "YYYYMMDD";
+    const formatAllDay = 'YYYYMMDDTHHmms';
+    const formatSingleEvent = 'YYYYMMDD';
 
-    if(moment(event.startDate).hour() === 0) {
-      _startDateBody = 'DTSTART;VALUE=DATE:' + moment(event.startDate).format(format_singleEvent) + '\n';
+    if (moment(event.startDate).hour() === 0) {
+        _startDateBody = `DTSTART;VALUE=DATE:${moment(event.startDate).format(formatSingleEvent)}\n`;
     } else {
-      _startDateBody = 'DTSTART:' + moment(event.startDate).format(format_allDay) + 'Z\n';
+        _startDateBody = `DTSTART:${moment(event.startDate).format(formatAllDay)}Z\n`;
     }
 
-    if(moment(event.endDate).hour() === 0) {
-      _endDateBody = 'DTEND;VALUE=DATE:' + moment(event.endDate).add(1, 'days').format(format_singleEvent) + '\n';
+    if (moment(event.endDate).hour() === 0) {
+        _endDateBody = `DTEND;VALUE=DATE:${moment(event.endDate).add(1, 'days').format(formatSingleEvent)}\n`;
     } else {
-      _endDateBody = 'DTEND:' + moment(event.endDate).format(format_allDay) + 'Z\n';
+        _endDateBody = `DTEND:${moment(event.endDate).format(formatAllDay)}Z\n`;
     }
 
 
-    body += _startDateBody +
-            _endDateBody +
-            'END:VEVENT\n' +
+    body += `${_startDateBody +
+            _endDateBody
+            }END:VEVENT\n` +
             'END:VCALENDAR';
 
-    var options = {
-      rejectUnauthorized: false,
-      hostname          : host,
-      port              : port,
-      path              : path,
-      method            : method,
-      headers           : {
-        "Content-type"  : "text/calendar",
-        "Content-Length": body.length,
-        "User-Agent"    : "calDavClient",
-        "Connection"    : "close",
-        "Depth"         : "1"
-      }
+    const options = {
+        rejectUnauthorized: false,
+        hostname: host,
+        port,
+        path,
+        method,
+        headers: {
+            'Content-type': 'text/calendar',
+            'Content-Length': body.length,
+            'User-Agent': 'calDavClient',
+            Connection: 'close',
+            Depth: '1',
+        },
     };
 
     if (user && pass) {
-      var userpass = new Buffer(user + ":" + pass).toString('base64');
-      options.headers["Authorization"] = "Basic " + userpass;
+        const userpass = Buffer.from(`${user}:${pass}`).toString('base64');
+        options.headers.Authorization = `Basic ${userpass}`;
     }
 
     /* ERROR example
@@ -100,18 +100,18 @@ var updateEvent = function (event, url, user, pass, method, cb) {
       </D:error>
     */
 
-    var err;
-    var req = https.request(options, function (res) {
+    let err;
+    const req = https.request(options, (res) => {
         if (res.statusCode < 200 || res.statusCode >= 300) {
-            err = new Error('response error: ' + res.statusCode);
+            err = new Error(`response error: ${res.statusCode}`);
         }
     });
 
-    req.on('error', function (e) {
+    req.on('error', (e) => {
         err = e;
     });
 
-    req.on('close', function () {
+    req.on('close', () => {
         return cb(err);
     });
 
@@ -138,16 +138,15 @@ module.exports = {
    * @param  {function} cb
 
    */
-  getEvents: function (url, user, pass, start, end, cb) {
+    getEvents: function (url, user, pass, start, end, cb) {
+        const urlparts = /(https?)\:\/\/(.*?):?(\d*)?(\/.*\/?)/gi.exec(url);
+        const protocol = urlparts[1];
+        const host = urlparts[2];
+        const port = urlparts[3] || (protocol === 'https' ? 443 : 80);
+        const path = urlparts[4];
+        const endTimeRange = (end) ? ` end="${end}"` : '';
 
-    var urlparts = /(https?)\:\/\/(.*?):?(\d*)?(\/.*\/?)/gi.exec(url);
-    var protocol = urlparts[1];
-    var host = urlparts[2];
-    var port = urlparts[3] || (protocol === "https" ? 443 : 80);
-    var path = urlparts[4];
-    var endTimeRange = (end) ? ' end="'+end+'"' : "";
-
-    var xml = '<?xml version="1.0" encoding="utf-8" ?>\n' +
+        const xml = `${'<?xml version="1.0" encoding="utf-8" ?>\n' +
       '<C:calendar-query xmlns:D="DAV:" xmlns:C="urn:ietf:params:xml:ns:caldav">\n' +
       '  <D:prop>\n' +
       '    <C:calendar-data/>\n' +
@@ -155,74 +154,71 @@ module.exports = {
       '  <C:filter>\n' +
       '    <C:comp-filter name="VCALENDAR">\n' +
       '      <C:comp-filter name="VEVENT">\n' +
-      '        <C:time-range start="'+start+'"'+endTimeRange+'/>\n' +
+      '        <C:time-range start="'}${start}"${endTimeRange}/>\n` +
       '      </C:comp-filter>\n' +
       '    </C:comp-filter>\n' +
       '  </C:filter>\n' +
       '</C:calendar-query>';
 
-    var options = {
-      rejectUnauthorized: false,
-      hostname          : host,
-      port              : port,
-      path              : path,
-      method            : 'REPORT',
-      headers           : {
-        "Content-type"  : "text/xml",
-        "Content-Length": xml.length,
-        "User-Agent"    : "calDavClient",
-        "Connection"    : "close",
-        "Depth"         : "1"
-      }
-    };
+        const options = {
+            rejectUnauthorized: false,
+            hostname: host,
+            port,
+            path,
+            method: 'REPORT',
+            headers: {
+                'Content-type': 'text/xml',
+                'Content-Length': xml.length,
+                'User-Agent': 'calDavClient',
+                Connection: 'close',
+                Depth: '1',
+            },
+        };
 
-    if (user && pass) {
-      var userpass = new Buffer(user + ":" + pass).toString('base64');
-      options.headers["Authorization"] = "Basic " + userpass;
-    }
+        if (user && pass) {
+            const userpass = Buffer.from(`${user}:${pass}`).toString('base64');
+            options.headers.Authorization = `Basic ${userpass}`;
+        }
 
-    var req = https.request(options, (res) => {
-      var s = "";
-      res.on('data', (chunk) => {
-        s += chunk;
-      });
+        let s = '';
+        const req = https.request(options, (res) => {
+            res.on('data', (chunk) => {
+                s += chunk;
+            });
+        });
 
-      req.on('close', () => {
-        var reslist = [];
-        try {
+        let err;
+        req.on('error', (e) => {
+            err = e;
+        });
 
-          console.log(s);
-          parseString(s, (err, result) => {
-            console.log("Parsing.....");
-            console.log(result);
-            var data = result['D:multistatus']['D:response'];
-
-            if(data) {
-              data.map((event) => {
-                var ics = event['D:propstat'][0]['D:prop'][0]['C:calendar-data'][0];
-                var jcalData = ICAL.parse(ics);
-                var vcalendar = new ical.Component(jcalData);
-                var vevent = vcalendar.getFirstSubcomponent('vevent');
-                reslist.push(vevent)
-              });
+        req.on('close', () => {
+            if (err) {
+                return cb(err);
             }
 
-            cb(reslist);
-          });
-        }
-        catch (e) {
-          console.log("Error parsing response");
-          console.log(e);
-        }
+            const reslist = [];
+            try {
+                return parseString(s, (err, result) => {
+                    const data = result['D:multistatus']['D:response'];
 
-      });
-    });
+                    if (data) {
+                        data.forEach((event) => {
+                            const ics = event['D:propstat'][0]['D:prop'][0]['C:calendar-data'][0]._;
+                            const jcalData = ical.parse(ics);
+                            const vcalendar = new ical.Component(jcalData);
+                            const vevent = vcalendar.getFirstSubcomponent('vevent');
+                            reslist.push(vevent);
+                        });
+                    }
 
-    req.end(xml);
+                    return cb(null, reslist);
+                });
+            } catch (e) {
+                return cb(e);
+            }
+        });
 
-    req.on('error', function (e) {
-      console.log('problem with request: ' + e.message);
-    });
-
-  }
+        return req.end(xml);
+    },
 };
